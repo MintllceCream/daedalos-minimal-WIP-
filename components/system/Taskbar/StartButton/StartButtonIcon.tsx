e18af228fd2e290bec9578faefbbece6b72ae36a
MintIIceCream <hoppynnap@gmail.com>
2026-07-18 23:42:24 -0500

    Update StartButtonIcon.tsx 2

--- components/system/Taskbar/StartButton/StartButtonIcon.tsx
+++ components/system/Taskbar/StartButton/StartButtonIcon.tsx
@@ -2,15 +2,15 @@ import { memo } from "react";
 
 const StartButtonIcon = memo(() => (
   <svg 
-    viewBox="0 0 24 24" 
-    xmlns="http://www.w3.org/2000/svg"
     fill="none" 
+    height="100%"
     stroke="currentColor" 
-    strokeWidth="2" 
     strokeLinecap="round" 
     strokeLinejoin="round"
+    strokeWidth="2" 
     width="100%" 
-    height="100%"
+    viewBox="0 0 24 24" 
+    xmlns="http://www.w3.org/2000/svg"
   >
     <path d="m14 10 7-7"/>
     <path d="M20 10h-6V4"/>
